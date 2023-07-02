aa42c76fb14c6dd4b3011464fac1301bca716a5c
Cliff Chan <cliffchan1993@gmail.com>
2023-07-02 11:48:05 +0800

    Initial Commit

---
+++ Splitpay/SplitpayApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitpayApp.swift
+//  Splitpay
+//
+//  Created by Cliff Chan on 2/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct SplitpayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
